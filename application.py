729d63630677983e294f09d812ceb8e6b7c5d3c4
thomas <thomassalcedo@hotmail.fr>
2021-10-27 13:48:44 +0200

    Ajout d'un programme ayant pour but de controler le lancement des programmes de recolte de donnees

---
+++ application.py
@@ -0,0 +1,14 @@
+from bs4.element import Script
+from Script_meteo import Combinaison
+from Script_instagram import donnees_insta_par_lieu
+from Script_instagram import nb_visiteurs_par_mois
+
+
+def main():
+    Combinaison.main() # Lance le script de la meteo
+    donnees_insta_par_lieu.main() # Lance le script instagram
+    nb_visiteurs_par_mois.main()
+
+
+if __name__ == "__main__":
+    main()
