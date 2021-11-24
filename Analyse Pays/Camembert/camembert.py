43501c88f90a422bb66aba4c39e3212ee5ab28f3
thomas <thomassalcedo@hotmail.fr>
2021-11-24 13:27:43 +0100

    Modification des camemberts

--- Analyse Pays/Camembert/camembert.py
+++ Analyse Pays/Camembert/camembert.py
@@ -69,18 +69,21 @@ def main():
         val = [dico[k] for k in label]
         acc = sum([dico[k] for k in label])  # Valeur totale de personne dans le top 5
 
-        """val = []
-        for lab in label:
-            val.append(dico[lab]/acc)"""
-
         autre = sum([dico[k] for k in dico if k not in label])
         val.append(autre)
 
-        plt.figure(figsize= (6,6))
+        plt.figure(figsize= (8,8))
 
-        plt.pie(val,labels=label+["Autres"],
-                      normalize=True,
-                      autopct = lambda x: str(round(x, 2)) + '%')
+        patches, texts, autotexts = plt.pie(val,
+                labels=label+["Autres"],
+                textprops={'fontsize': 13},
+                normalize=True,
+                autopct = lambda x: str(round(x, 2)) + '%',
+                pctdistance=0.83)
+
+        for k in autotexts:
+            k.set_color('white')
+            k.set_fontsize(16)
 
         plt.savefig(f"Camembert_{sortie[i]}.png")
 
@@ -93,7 +96,23 @@ def main():
             
 
 
+def test():
+    frac=[1.40 , 10.86 , 19.31 , 4.02 , 1.43 , 2.66 , 4.70 , 0.70 , 0.13 , 1.48, 32.96 , 1.11 , 13.30 , 5.86]
+    labels=['HO0900344', 'HO0900331', 'HO0900332', 'HO0900354', 
+            'HO0900358', 'HO0900374', 'HO0900372', 'HO0900373', 
+            'HO0900371', 'HO0900370', 'HO0900369', 'HO0900356', 
+            'HO0900353', 'HO0900343']
 
+    colors=('b', 'g', 'r', 'c', 'm', 'y', 'burlywood', 'w')
+
+    plt.figure(figsize=(8,8))
+    patches, texts, autotexts = plt.pie(frac, colors=colors, labels=labels, autopct='%1.1f%%',textprops={'fontsize': 14})
+    for i in autotexts:
+        i.set_color('white')
+        i.set_fontsize(30)
+    #autotexts = map(lambda t: t.set_fontsize(30),autotexts)
+    plt.show()
 
 if __name__ == "__main__":
     main()
+    #test()
